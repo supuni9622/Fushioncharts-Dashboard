53271f0567d4fec54ec2f21b5dbfc517982bd551
Supuni22 <supunimanamperi@gmail.com>
2022-05-15 13:05:45 +0530

    Created react app

---
+++ fusion-dashboard/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from "./headerComponent/Header";
+import ChartsView from "./ChartsView";
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <ChartsView/>
+    </div>
+  );
+}
+
+export default App;
